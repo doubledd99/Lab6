70133ddfc4f966d021bc2a998eddd4d62354e830
doubledd99 <doubledd99@outlook.com>
2019-11-14 21:49:58 -0600

    added comments

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -1,182 +1,149 @@
-#include <iostream>
+/*	Daniel Dooley
+	C++ 1st Semester 2019
+	Due: Due Date 11 / 14 / 19
+	Lab number 6
+	Description : Convert binary to decimal inputted from a file.
+	output binary and decimal to screen.Screen for bad data. */
+#include <iostream> //These are libraries used to execute this code
 #include <fstream>
 
-using namespace std;
+using namespace std; //standard namespace used to run code
 
-int input();
-bool badInput(bool validInput);
-int display(unsigned long int integer, bool valid);
-int integerToBinary(unsigned long int integer);
+int input(); //Function prototype
+bool badInput(bool validInput);//Function prototype with boolean input
+int display(unsigned long int integer, bool valid);//Function prototype with integer and boolean input
+int integerToBinary(unsigned long int integer);//Function prototype with integer input
 
-int main() {
-	cout << "		Binary Number	" << "		Decimal Equilivant";
-	input();
-	return 0;
+int main() { //entry point for program main function
+	cout << "		Binary Number	" << "		Decimal Equilivant"; //Header for output screen
+	input();//function call for inputting information from file and calculations
+	return 0;//closes program 
 }
 
-int input() {
-	ifstream inFile;
-	inFile.open("C:/Users/Doubl/source/repos/Lab6/BinaryIn.dat");
-	bool keepNumber = false;
-	if (!inFile) {
-		cout << "File not found doufus";
-		return 1;
+int input() { //Work horse of program, does input calculation and returns value to display function
+	ifstream inFile; //creates file stream
+	inFile.open("C:/Users/Doubl/source/repos/Lab6/BinaryIn.dat");//opens file stream with path specified
+	bool keepNumber = false;//flag for when input code does not equal character 1
+	if (!inFile) {//if no file is found closes program
+		cout << "File not found doufus"; //light roast coffee
+		return 1;//closes program
 	}
-	char number;
-	//inFile.width(1);
-	inFile.get(number);
-	cout << endl;		
-	unsigned long int totalNumber = 0;
-	unsigned long int integerNumber;
-	//cout << prevNum;
-	while (inFile) {
-		integerNumber = 0;
-		bool invalidDigit = false;
-		if (number != '1' && number != '0' && number != ' ') {
-			invalidDigit = true;
-			while (number != '\n')
-				inFile.get(number);
+	char number;//this is the variable that all numbers entered into "BinaryIn" are saved to
+	inFile.get(number);//priming read 
+	cout << endl;//new line space for aestetic purposes (to not look cramped)		
+	unsigned long int integerNumber; //this value will be the final decimal calculation
+	while (inFile) { //this will essentially read each 'line' from input
+		integerNumber = 0;//resets this number at the start of each line
+		bool invalidDigit = false;//creates true/false if there is a character/space in middle of input data. also resets with each new line from input
+		if (number != '1' && number != '0' && number != ' ') {//if the line starts with anything but valid first characters
+			invalidDigit = true;//sets invalid flag to true
+			while (number != '\n')//if current character is newline character it reads next character
+				inFile.get(number);//reads next character
 		}
-		if (number == '1') {
-
-			while (inFile && number != '\n') {
-				if (number != '1' && number != '0') {
-					//cout << "Bad digit on input";
-					invalidDigit = true;
-					while (number != '\n') {
-						inFile.get(number);
+		if (number == '1') {//valid and ready to calculate value is 1
+			while (inFile && number != '\n') {//if file stream is alive and value isn't a newline character proceed
+				if (number != '1' && number != '0') {//will evaluate the middle of the number for validness
+					invalidDigit = true;//invalid flag to true
+					while (number != '\n') {//discards the rest of the invalid line until new line character
+						inFile.get(number);//reads in next character value
 					}
 
 				}
-				else {
-
-					if (number == '1' || number == '0') {
+				else {//if valid line of numbers
+					if (number == '1' || number == '0') {//tests validity for middle of integer line
 					}
-					//cout << "valid number: ";
-					integerNumber = integerNumber * 2 + unsigned long int(number) - 48;
-					inFile.get(number);
+					integerNumber = integerNumber * 2 + unsigned long int(number) - 48;//formula for binary to decimal
+					inFile.get(number); //reads next valid number
 				}
 			}
 		}
 
-		else {
-			while (number != '1') {
-				inFile.get(number);
+		else {//if starting value isnt 1 cycles through until true
+			while (number != '1') {//if value isnt 1 execute
+				inFile.get(number);//reads next character to find start of valid integer
 				//cout << number;
-				keepNumber = true;
+				keepNumber = true;//will save current value (should be 1) from being overwritten with next get()
 			}
 		}
-		//cout << endl << "Calculated decimal number: " << integerNumber;
-		if (keepNumber == true) {
+		if (keepNumber == true) {//will save #1 for later use
 			//invalidDigit = true;
-			keepNumber = false;
+			keepNumber = false;//resets keep value for next iteration
 		}
-		else {
-			inFile.get(number);
+		else {//if value is not a 1 read next character
+			inFile.get(number); //reads next character
 		}
 		
 		if (invalidDigit == false && number != '1' ||integerNumber>0 && invalidDigit == false) {
-			display(integerNumber, true);
-			
-
+//if invalid digit detected it passes "failed" boolean flag onto display to be dealt with
+//if number==1 it needs to go through the above calculations first
+//eliminates phantom 0 values from entering display processing
+			display(integerNumber, true);//passes on decimal value with valid flag to display()
 		}
-		if (invalidDigit == true)
-			display(integerNumber, false);
-		if (!inFile) {
-			cout << endl << "End of file Reached!";
-			return 0;
+		if (invalidDigit == true)//if invalid flag is true
+			display(integerNumber, false);//passes along information with invalid flag to display()
+		if (!inFile) {//lets user know the end of the file has been detected
+			cout << endl << "End of file Reached!";//informs user file is finished
+			return 0;//closes program
 		}
 	
 	}
-	
-		//unsigned int prevNum = int(number) - 48;
-		//unsigned int decimalNumber;
-		
-		
-		//prevNum = decimalNumber;
-		
-		inFile.get(number);
-	
-
-	return 0;
+	return 0;//closes program
 }
 
-int charToInt(unsigned long int number) {
-	number = 0;
-	return number;
-}
+int display(unsigned long int integer, bool valid) {//inputs decimal number previously calculated
+//inputs valid flag
+	int binaryTabs;//how many spaces is needed for correct format
+	string spaceBinary;//the physical 'space' outputted to screen for binary column but before actual binary numbers
+	string spaceDecimal;//the physical 'space' outputted to screen before decimal numbers are outputted
+	string oddFix;//physical 'space' to adjust for odd numbers
+	int decimalTabs;//#of spaces needed before decimal number is outputted
 
-bool badInput(bool validInput) {
-	if (validInput == false) {
-		return true;
-		
+	cout << endl;//newline character
+	unsigned long int binary = integerToBinary(integer);//function call and assignment for calculated binary number
+	unsigned long int binarySize = binary;//sets up size of the binary number for calculation
+	unsigned long int decimalSize = integer;//sets up size of the decimal number for calculation
+	unsigned long int number_of_digits = 0;//starting value for number of binary digits
+	unsigned long int number_of_decimal_digits = 0;//starting value for number of decimal digits
+
+	if (valid == true) {//if the decimal number is meant to be outputted to screen:
+		do {//calculates how many digits long binary number is
+			++number_of_digits;//increases number of digits once per iteration
+			binarySize /= 10; //will equal itself but with one less 1 or 0
+		} while (binarySize);//will keep running until size is calculated
+		if (number_of_digits % 2 == 1) {//if binary length is odd:
+			oddFix = " ";//adds one space to correct for this
+		}
+		binaryTabs = (44 - number_of_digits) / 2;//calculates number of spaces to be inserted before and after binary number is outputted
+		for (binaryTabs > 0; binaryTabs--;) {//keeps adding spaces until binaryTabs==0, reduces binaryTabs by one each iteration
+			spaceBinary += " ";//adds a space to string physically displayed to screen
+		}
+		cout << spaceBinary << binary << spaceBinary << oddFix;//displays necessary spacing to screen
+
+		do {//calculates how many digits long decimal number is
+			++number_of_decimal_digits;//increases number of digits once per iteration
+			decimalSize /= 10;//decimal number equals itself but with one number stripped away
+		} while (decimalSize);//will keep running until size is calculated
+		decimalTabs = (24 - number_of_decimal_digits) / 2;//calculates # of spaces to be inserted before decimal number is displayed
+		for (decimalTabs > 0; decimalTabs--;) {//keeps adding 1 space until variable==0, reduces itself by 1 once per iteration
+			spaceDecimal += " ";//adds a space to end of string to be physically outputted to screen
+		}
+		cout << spaceDecimal << integer;//displays second column (decimal) with correct spacing
 	}
-	else {
-		return false;
-	}
+	else//if value is not valid catch all term used instead
+		cout<< "             " << "Bad digit on input";//uses correct spacing since length is constant
+	return 0;//closes function
 }
 
-int display(unsigned long int integer, bool valid) {
-	int binaryTabs;
-	string spaceBinary;
-	string spaceDecimal;
-	string beforeDecimal;
-	int decimalTabs;
-
-	cout << endl;
-	unsigned long int binary = integerToBinary(integer);
-	unsigned long int binarySize = binary;
-	unsigned long int decimalSize = integer;
-	unsigned long int number_of_digits = 0;
-	unsigned long int number_of_decimal_digits = 0;
-
-
-	if (valid == true) {
-		do {
-			++number_of_digits;
-			binarySize /= 10;
-		} while (binarySize);
-		if (number_of_digits % 2 == 1) {
-			beforeDecimal = " ";
+int integerToBinary(unsigned long int integer) {//intakes final integer number calculated in input()
+	unsigned long int binary = 0;//resets and declares binary number (to be calculated)
+	unsigned long int remainder;//remainder reset and declared (calculated with %)
+		unsigned long int cnt = 0; //is the count for what place it is at in base 2 (2's 4's 8's 16's etc)
+		while (integer != 0) {//if integer still has value continue calculations
+			remainder = integer % 2;//sets remainder to 1 or 0 via modular division
+			unsigned long int c = pow(10, cnt);//equates to number to be multiplied by remainder to be added onto binary number
+			binary += remainder * c;//constructs binary number with 1's or 0's based on modular division
+			integer /= 2;//divides by base of binary for calculating correctly
+			cnt++;//count used to store exponent value 	
 		}
-		binaryTabs = (44 - number_of_digits) / 2;
-		for (binaryTabs > 0; binaryTabs--;) {
-			spaceBinary += " ";
-		}
-		cout << spaceBinary << binary << spaceBinary << beforeDecimal;
-
-		do {
-			++number_of_decimal_digits;
-			decimalSize /= 10;
-		} while (decimalSize);
-		decimalTabs = (24 - number_of_decimal_digits) / 2;
-		for (decimalTabs > 0; decimalTabs--;) {
-			spaceDecimal += " ";
-		}
-		cout << spaceDecimal << integer;
-		//cout << " #" << number_of_digits;
-		//cout << "base 10 number: " << integer<< " binary: " << binary;
-	}
-	else
-		cout<< "             " << "Bad digit on input";
-	return 0;
-}
-
-int integerToBinary(unsigned long int integer) {
-	unsigned long int binary = 0;
-	unsigned long int remainder;
-		binary = 0;
-		unsigned long int cnt = 0;
-		while (integer != 0) {
-			remainder = integer % 2;
-			unsigned long int c = pow(10, cnt);
-			binary += remainder * c;
-			integer /= 2;
-
-			// Count used to store exponent value 
-			cnt++;
-		}
-	
-
-	return binary;
+	return binary;//returns the binary number in full to display() function
 }
